51adbed9e6fd229e5dbc570d7e27aee7f3bb2415
sibun <sibasankardash977@gmail.com>
2026-02-05 12:21:51 +0530

    login form created

--- CustomerSide/login.php
+++ CustomerSide/login.php
@@ -4,5 +4,14 @@
 </head>
 <body>
     //write your code here
+    <form action="post">
+        <div>
+            Enter your email : 
+            <input type="text" name="email" value="email"><br>
+            Enter your password :
+            <input type="password" name="password" value="password"><br>
+            <input type="submit" value="Login">
+        </div>
+    </form>
 </body>
 </html>
