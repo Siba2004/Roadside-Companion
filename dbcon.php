a5edd7a395b3ec2d934dab8d3d81a97194c127ec
ajay264 <ajay6@gmail.com>
2026-02-18 13:10:59 +0530

    DBcon changes

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$conn=new mysqli("localhost","root","","road_side_companion","3306");
+$conn=new mysqli("localhost","root","2255","road_side_companion","3306");
 if($conn->connect_error){
     die("Error : ".$conn->connect_error);
 }
